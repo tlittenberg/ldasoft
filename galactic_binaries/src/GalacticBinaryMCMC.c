8be133f1051c1b0433d72b59b7a7359f4a5e7f29
lackeos <alackeos@gmail.com>
2019-04-08 14:14:29 -0500

    debug cov matrix prop  Please enter the commit message for your changes. Lines starting

--- galactic_binaries/src/GalacticBinaryMCMC.c
+++ galactic_binaries/src/GalacticBinaryMCMC.c
@@ -177,9 +177,9 @@ int main(int argc, char *argv[])
         else if(flags->update || flags->updateCov)
         {
           if(flags->update)
-            draw_from_cdf(data_ptr, model_ptr, model_ptr->source[n], proposal[i][7], model_ptr->source[n]->params , chain->r[ic]);
+            draw_from_cdf(data_ptr, model_ptr, model_ptr->source[n], proposal[i][6], model_ptr->source[n]->params , chain->r[ic]);
 //          if(flags->updateCov)
-//            draw_from_cov(data_ptr, model_ptr, model_ptr->source[n], proposal[i][6], model_ptr->source[n]->params , chain->r[ic]);
+//            draw_from_cov(data_ptr, model_ptr, model_ptr->source[n], proposal[i][7], model_ptr->source[n]->params , chain->r[ic]);
         }
         else
         {
