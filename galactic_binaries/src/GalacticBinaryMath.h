ac84c9564d04104228fa91b42de92017cac441aa
Tyson Littenberg <tyson.littenberg@ligo.org>
2019-09-23 12:37:54 -0500

    change brute force w/ binary search in cdf density

--- galactic_binaries/src/GalacticBinaryMath.h
+++ galactic_binaries/src/GalacticBinaryMath.h
@@ -22,20 +22,23 @@ double ipow(double x, int n);
 double fourier_nwip(double *a, double *b, double *Sn, int n);
 double snr(struct Source *source, struct Noise *noise);
 
+int binary_search(double *array, int nmin, int nmax, double x);
+
 void matrix_eigenstuff(double **matrix, double **evector, double *evalue, int N);
 void invert_matrix(double **matrix, int N);
 void cholesky_decomp(double matrix[8][8], double **tensor_out, int N);
 void invert_matrix2(double matrix[8][8], double **tensor_out, int N);
 
 /* ********************************************************************************** */
-/*                                                                                      */
+/*                                                                                    */
 /*                                    Fourier Tools                                   */
-/*                                                                                      */
+/*                                                                                    */
 /* ********************************************************************************** */
 
 void dfour1(double data[], unsigned long nn, int isign);
 void drealft(double data[], unsigned long n, int isign);
 
 double power_spectrum(double *data, int n);
+
 
 #endif /* GalacticBinaryMath_h */
