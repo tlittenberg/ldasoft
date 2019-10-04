cbc3591b03049ae0144fbc444d3aca91c780bcdc
Tyson Littenberg <tyson.littenberg@ligo.org>
2019-10-04 12:38:15 -0500

    some saftey checks for cov proposal

--- galactic_binaries/src/GalacticBinaryMCMC.c
+++ galactic_binaries/src/GalacticBinaryMCMC.c
@@ -174,11 +174,11 @@ int main(int argc, char *argv[])
         }
         else if(flags->updateCov)
         {
-          draw_from_cov(data_ptr, model_ptr, model_ptr->source[n], proposal[i][7], model_ptr->source[n]->params , chain->r[ic]);
+          while ( !isfinite(draw_from_cov(data_ptr, model_ptr, model_ptr->source[n], proposal[i][7], model_ptr->source[n]->params , chain->r[ic])));
         }
         else if(flags->update)
         {
-          draw_from_cdf(data_ptr, model_ptr, model_ptr->source[n], proposal[i][7], model_ptr->source[n]->params , chain->r[ic]);
+          draw_from_cdf(data_ptr, model_ptr, model_ptr->source[n], proposal[i][6], model_ptr->source[n]->params , chain->r[ic]);
         }
         else
         {
