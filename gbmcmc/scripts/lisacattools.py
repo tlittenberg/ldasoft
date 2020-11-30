9d5ff68e59d000edafe2f92f15e8e413381db114
Cloud User <centos@ip-172-31-94-212.ec2.internal>
2020-11-30 19:35:37 +0000

    added HEALPix tools and did some cleaning

--- gbmcmc/scripts/lisacattools.py
+++ gbmcmc/scripts/lisacattools.py
@@ -9,6 +9,11 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
 
+try: 
+    import healpy as hp  #healpy for healpix sky-location binning. Not as commonly installed so trying this as a template for an optional module
+except ImportError: 
+    hp = None
+
 # constants
 ep_const = 23.4 # obliquity of the ecliptic (degrees)
 a_G_const = 192.85948 # r.a. of galacitc North Pole (degrees)
@@ -17,30 +22,18 @@ l_NCP_const = 122.93192 # galactic longitude of North Celestial Pole (degrees)
 
 ## Sky Coordinate Funcitons
 
-# deg2rad
-def deg2rad(degrees):
-    # Convert degrees to radians
-    radians = degrees*np.pi/180.0
-    return radians
-
-# rad2deg
-def rad2deg(radians):
-    # Convert radians to degrees
-    degrees = radians * 180 / np.pi
-    return degrees
-
 # eclp2eqi
 def eclp2eqi(lamb,beta,ep=ep_const):  
     # Convert ecliptic to equitorial coordinates
     # the arguments of the function are ecliptic longitude lambda, ecliptic latitude beta, and the obliquity of the ecliptic (all in degrees)
-    ep = deg2rad(ep)
-    lamb = deg2rad(lamb)
-    beta = deg2rad(beta)
+    ep = np.deg2rad(ep)
+    lamb = np.deg2rad(lamb)
+    beta = np.deg2rad(beta)
     alpha = np.arctan2(np.cos(beta) * np.sin(lamb) * np.cos(ep) - np.sin(beta) * np.sin(ep), np.cos(beta) * np.cos(lamb))
     delta = np.arcsin(np.sin(beta) * np.cos(ep) + np.cos(beta) * np.sin(ep) * np.sin(lamb))
 
-    alpha = rad2deg(alpha)
-    delta = rad2deg(delta)
+    alpha = np.rad2deg(alpha)
+    delta = np.rad2deg(delta)
     
     return alpha,delta
 
@@ -48,17 +41,17 @@ def eclp2eqi(lamb,beta,ep=ep_const):
 def eqi2gal(alpha, delta, a_G = a_G_const, d_G = d_G_const, l_NCP = l_NCP_const):
     # Convert equitorial to galactic coordinates 
     # arguments are right ascension, declination, r.a. of glactic NP, dec of galactic NP, and galactic longitude of north celestial pole (all in degrees)
-    a_G = deg2rad(a_G)
-    d_G = deg2rad(d_G)
-    l_NCP = deg2rad(l_NCP)
-    alpha = deg2rad(alpha)
-    delta = deg2rad(delta)
+    a_G = np.deg2rad(a_G)
+    d_G = np.deg2rad(d_G)
+    l_NCP = np.deg2rad(l_NCP)
+    alpha = np.deg2rad(alpha)
+    delta = np.deg2rad(delta)
     
     b = np.arcsin(np.sin(delta) * np.sin(d_G) + np.cos(delta) * np.cos(d_G) * np.cos(alpha - a_G))
     l = l_NCP - np.arctan2(np.cos(delta) * np.sin(alpha - a_G), np.sin(delta) * np.cos(d_G) - np.cos(delta) * np.sin(d_G) * np.cos(alpha - a_G))
     
-    b = rad2deg(b)
-    l  = rad2deg(l)
+    b = np.rad2deg(b)
+    l  = np.rad2deg(l)
     return l,b
 
 # eclp2gal
@@ -75,11 +68,15 @@ def getGalcoord(df, ep = ep_const, a_G = a_G_const, d_G = d_G_const, l_NCP = l_N
     # convert Pandas dataframe (either of sources or a chain) from Ecliptic to Galactic Coords
     lamb = df['Ecliptic Longitude']
     beta = np.pi/2-np.arccos(np.array(df['coslat']))
-    lamb = rad2deg(lamb)
-    beta = rad2deg(beta)
+    lamb = np.rad2deg(lamb)
+    beta = np.rad2deg(beta)
     l,b = eclp2gal(lamb,beta)
-    df.insert(len(df.columns),'Galactic Longitude',l,True)
-    df.insert(len(df.columns),'Galactic Latitude',b,True)
+    if not('Galactic Latitude' in df.columns):
+        df.insert(len(df.columns),'Galactic Longitude',l,True)
+        df.insert(len(df.columns),'Galactic Latitude',b,True)
+    else :
+        df['Galactic Longitude'] = l
+        df['Galactic Latitude'] = b
     return 
 
 # ellipse_area
@@ -122,6 +119,59 @@ def confidence_ellipse(df, ax, n_std=1.0, facecolor='none', **kwargs):
 
     ellipse.set_transform(transf + ax.transData)
     return ax.add_patch(ellipse)
+
+
+## HEALPix functions using healpy
+if hp is None:
+    print("Warning: healpy module not found, HEALPIX functions will not be available.")
+else :
+
+    def HPbin(df,nside, system = 'Galactic'):
+        # Assigns each lat/lon coordinate to a HEALPPIX Bin. Optional argument 'system' can be 'Galactic' [default] or 'Ecliptic'
+        
+        # load in the coordinates in radians
+        if system == 'Galactic':
+            if not('Galactic Latitude' in df.columns):
+                getGalcoord(df)
+                
+            lat = np.deg2rad(np.array(df['Galactic Latitude']))
+            lon = np.deg2rad(np.array(df['Galactic Longitude']))
+        elif system == 'Ecliptic':
+            lat = np.deg2rad(np.array(df['Ecliptic Latitude']))
+            lon = np.deg2rad(np.array(df['Ecliptic Longitude']))
+        else: 
+            print('%s is not a valid coordinate system, please choose Galactic or Ecliptic' % system)
+            return
+        
+        # make the healpix map and insert into the passed dataframe
+        hpidx = hp.ang2pix(nside,np.pi/2.0-lat,lon)  # the latitude/co-latitude convention in HEALPY
+        if not('HEALPix bin' in df.columns):
+            df.insert(len(df.columns),'HEALPix bin',hpidx,True)
+        else :
+            df['HEALPix bin'] = hpidx
+        return
+    
+    def HPhist(df,nside,system = 'Galactic'):
+        # performs HEALPix binning of a set of sky localization coordinates and returns an array of counts
+
+        # first check that HEALPix mapping has occurred and do it if necessary
+        if not('HEALPix bin' in df.columns):
+            HPbin(df,nside, system)
+          
+        # make an empty array for all the bins 
+        npix = hp.nside2npix(nside)
+        hp_map = np.zeros(npix,dtype=int)
+        # count samples in the non-empty bins    
+        hp_idx, hp_cnts = np.unique(df['HEALPix bin'],return_counts = True)
+        # fill in the non-empty bins
+        hp_map[hp_idx] = hp_cnts
+        
+        return hp_map
+            
+            
+        
+        
+
 
 ## Luminosity Distance Functions
 
