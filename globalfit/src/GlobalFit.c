611ba2a373767094458ab8540009448b7e3bf9d1
Tyson Littenberg <tyson.littenberg@ligo.org>
2021-02-11 16:27:01 -0600

    adding global fit codes

---
+++ globalfit/src/GlobalFit.c
@@ -0,0 +1,238 @@
+//
+//  GlobalFit.c
+//  
+//
+//  Created by Tyson Littenberg on 1/26/21.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+
+#include <mpi.h>
+#include <omp.h>
+
+#include <LISA.h>
+
+#include <GalacticBinary.h>
+#include <GalacticBinaryIO.h>
+#include <GalacticBinaryData.h>
+#include <GalacticBinaryPrior.h>
+#include <GalacticBinaryModel.h>
+#include <GalacticBinaryProposal.h>
+#include <GalacticBinaryWaveform.h>
+#include <GalacticBinaryMCMC.h>
+
+#include "GalacticBinaryWrapper.h"
+#include "NoiseWrapper.h"
+
+#define NMAX 10
+
+static void share_gbmcmc_residual(struct GBMCMCData *gbmcmc_data, struct NoiseData *noise_data, int GBMCMC_Flag, int Noise_Flag, int root)
+{
+    if(GBMCMC_Flag)
+    {
+        struct Data *data = gbmcmc_data->data;
+        struct Chain *chain = gbmcmc_data->chain;
+        struct Model *model = gbmcmc_data->model[chain->index[0]];
+        MPI_Send(model->residual[0]->A, 2*data->N, MPI_DOUBLE, root, 0, MPI_COMM_WORLD);
+        MPI_Send(model->residual[0]->E, 2*data->N, MPI_DOUBLE, root, 1, MPI_COMM_WORLD);
+    }
+    if(Noise_Flag)
+    {
+        MPI_Status status;
+        int N = gbmcmc_data->data->N*2;
+        int procID_min = gbmcmc_data->procID_min;
+        int procID_max = gbmcmc_data->procID_max;
+        struct Data *data = noise_data->data;
+        for(int n=procID_min; n<=procID_max; n++)
+        {
+            MPI_Recv(data->tdi[0]->A + (n-procID_min)*N, N, MPI_DOUBLE, n, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(data->tdi[0]->E + (n-procID_min)*N, N, MPI_DOUBLE, n, 1, MPI_COMM_WORLD, &status);
+        }
+    }
+}
+
+static void share_noise_model(struct GBMCMCData *gbmcmc_data, struct NoiseData *noise_data, int GBMCMC_Flag, int Noise_Flag, int root)
+{
+    if(Noise_Flag)
+    {
+        int procID_min = gbmcmc_data->procID_min;
+        int procID_max = gbmcmc_data->procID_max;
+
+        struct Chain *chain = noise_data->chain;
+        struct Model *model = noise_data->model[chain->index[0]];
+        for(int n=procID_min; n<=procID_max; n++)
+        {
+            
+            MPI_Send(&model->noise[0]->etaA, 1, MPI_DOUBLE, n, 0, MPI_COMM_WORLD);
+            MPI_Send(&model->noise[0]->etaE, 1, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
+        }
+    }
+    if(GBMCMC_Flag)
+    {
+        MPI_Status status;
+        struct Data *data = gbmcmc_data->data;
+        struct Chain *chain = gbmcmc_data->chain;
+        struct Model *model = gbmcmc_data->model[chain->index[0]];
+        
+        MPI_Recv(&model->noise[0]->etaA, 1, MPI_DOUBLE, root, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&model->noise[0]->etaE, 1, MPI_DOUBLE, root, 1, MPI_COMM_WORLD, &status);
+        
+        //copy new noise parameters to each chain & update PSD
+        for(int i=0; i<chain->NC; i++)
+        {
+            gbmcmc_data->model[chain->index[i]]->noise[0]->etaA = model->noise[0]->etaA;
+            gbmcmc_data->model[chain->index[i]]->noise[0]->etaE = model->noise[0]->etaE;
+            generate_noise_model(data, gbmcmc_data->model[chain->index[i]]);
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    time_t start, stop;
+    start = time(NULL);
+
+    int Nproc, procID;
+    int root = 0; //root process
+
+    MPI_Init(&argc, &argv); //start parallelization
+    
+    /* get process ID, and total number of processes */
+    MPI_Comm_size(MPI_COMM_WORLD, &Nproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+
+    /* root process check command line */
+    if(procID==root)
+    {
+        //check command line format
+        print_LISA_ASCII_art(stdout);
+        if(argc==1) print_usage();
+        
+    } MPI_Barrier(MPI_COMM_WORLD);
+
+    /* Allocate data structures */
+    struct GBMCMCData *gbmcmc_data = malloc(sizeof(struct GBMCMCData));
+    alloc_gbmcmc_data(gbmcmc_data, procID, 1, Nproc-1);
+    
+    struct NoiseData *noise_data = malloc(sizeof(struct NoiseData));
+    alloc_noise_data(noise_data, procID);
+    
+    /* Aliases to gbmcmc structures */
+    struct Flags *flags = gbmcmc_data->flags;
+    struct Orbit *orbit = gbmcmc_data->orbit;
+    struct Chain *chain = gbmcmc_data->chain;
+    struct Data *data   = gbmcmc_data->data;
+
+    /* all processes parse command line and set defaults/flags */
+    parse(argc,argv,data,orbit,flags,chain,NMAX,Nproc,procID);
+    
+    /* Finish allocating GBMCMC structures now that we know the number of PT chains */
+    gbmcmc_data->proposal = malloc(chain->NP*sizeof(struct Proposal*));
+    gbmcmc_data->model = malloc(sizeof(struct Model*)*chain->NC);
+    gbmcmc_data->trial = malloc(sizeof(struct Model*)*chain->NC);
+
+    /* Initialize data structures */
+    alloc_data(data, flags);
+        
+    /* TDI structure to hold full dataset */
+    struct TDI *tdi_full = malloc(sizeof(struct TDI));
+
+    /* root process reads data */
+    if(procID==root) GalacticBinaryReadHDF5(data,tdi_full);
+
+    /* broadcast data to all gbmcmc processes and select frequency segment */
+    get_frequency_segment(gbmcmc_data->data, tdi_full, tdi_full->N, root, procID);
+
+    /* set up data for noise model processes */
+    if(procID==root) setup_noise_data(noise_data, gbmcmc_data, tdi_full);
+
+    /* Initialize LISA orbit model */
+    initialize_orbit(data,orbit,flags);
+
+    /*
+     * Initialize all of the samplers
+     *
+     */
+
+    //choose which sampler to run based on procID
+    int GBMCMC_Flag = 0;
+    int Noise_Flag = 0;
+
+    /* Assign processes to GBMCMC model */
+    if(procID >= gbmcmc_data->procID_min && procID <= gbmcmc_data->procID_max)
+    {
+        GBMCMC_Flag = 1;
+        initialize_gbmcmc_sampler(gbmcmc_data);
+    }
+    
+    /* ssign processes to Noise model */
+    if(procID == root) //TODO: generalize so that noise model doesn't have to be root
+    {
+        Noise_Flag = 1;
+        initialize_noise_sampler(noise_data);
+    }
+    
+    
+    /*
+     * Master Blocked Gibbs sampler
+     *
+     */
+    do
+    {
+        /* ============================= */
+        /*     ULTRACOMPACT BINARIES     */
+        /* ============================= */
+
+        /* gbmcmc sampler gibbs update */
+        if(GBMCMC_Flag)
+            gbmcmc_data->status = update_gbmcmc_sampler(gbmcmc_data);
+
+        /* get global status of gbmcmc samplers */
+        gbmcmc_data->status = get_gbmcmc_status(gbmcmc_data,Nproc,root,procID);
+
+        /* share gbmcmc residual with other worker nodes */
+        share_gbmcmc_residual(gbmcmc_data, noise_data, GBMCMC_Flag, Noise_Flag, root);
+        
+        /* ============================= */
+        /*    INSTRUMENT NOISE MODEL     */
+        /* ============================= */
+
+        /* noise model update */
+        if(Noise_Flag)
+            noise_data->status = update_noise_sampler(noise_data);
+        
+        /* send noise model with other worker nodes */
+        share_noise_model(gbmcmc_data, noise_data, GBMCMC_Flag, Noise_Flag, root);
+        
+        /* ============================= */
+        /*  MASSIVE BLACK HOLE BINARIES  */
+        /* ============================= */
+
+        /* mbh model update */
+
+        /* share mbh residual with other worker nodes */
+
+    }while(gbmcmc_data->status!=0);
+    
+    
+    if(GBMCMC_Flag) print_waveforms_reconstruction(gbmcmc_data->data, gbmcmc_data->flags);
+    if(Noise_Flag)  print_noise_reconstruction(noise_data->data, noise_data->flags);
+    
+    /* clean up */
+    free_tdi(tdi_full);
+
+    //print total run time
+    stop = time(NULL);
+
+    if(procID==root) printf(" ELAPSED TIME = %g seconds on %i processes\n",(double)(stop-start),Nproc);
+
+    MPI_Finalize();//ends the parallelization
+
+    return 0;
+}
+
+
---
+++ globalfit/src/NoiseWrapper.c
@@ -0,0 +1,229 @@
+//
+//  NoiseWrapper.c
+//  
+//
+//  Created by Tyson Littenberg on 2/5/21.
+//
+
+#include <sys/stat.h>
+#include <string.h>
+
+#include <mpi.h>
+#include <omp.h>
+
+#include <LISA.h>
+#include <GalacticBinary.h>
+#include <GalacticBinaryIO.h>
+#include <GalacticBinaryData.h>
+#include <GalacticBinaryPrior.h>
+#include <GalacticBinaryModel.h>
+#include <GalacticBinaryProposal.h>
+#include <GalacticBinaryMCMC.h>
+
+#include "GalacticBinaryWrapper.h"
+#include "NoiseWrapper.h"
+
+void alloc_noise_data(struct NoiseData *noise_data, int procID)
+{
+    noise_data->status = 0;
+    noise_data->procID = procID;
+    noise_data->flags = NULL;//malloc(sizeof(struct Flags));
+    noise_data->orbit = NULL;//malloc(sizeof(struct Orbit));
+    noise_data->chain = malloc(sizeof(struct Chain));
+    noise_data->data  = malloc(sizeof(struct Data));
+}
+
+void setup_noise_data(struct NoiseData *noise_data, struct GBMCMCData *gbmcmc_data, struct TDI *tdi_full)
+{
+    int procID = noise_data->procID;
+    char dirname[MAXSTRINGSIZE];
+
+    noise_data->data->downsample = gbmcmc_data->data->downsample;
+    noise_data->data->Nwave      = gbmcmc_data->data->Nwave;
+    
+    
+    noise_data->chain->NC      = gbmcmc_data->chain->NC;
+    noise_data->data->Nchannel = gbmcmc_data->data->Nchannel;
+    noise_data->data->NP       = gbmcmc_data->data->NP;
+    strcpy(noise_data->data->format,gbmcmc_data->data->format);
+
+    int qpad = gbmcmc_data->data->qpad;
+    int N = gbmcmc_data->data->N - 2*qpad;
+    int Nseg = gbmcmc_data->procID_max - gbmcmc_data->procID_min + 1;
+    double T = gbmcmc_data->data->T;
+    
+    noise_data->data->T = T;
+    noise_data->data->N = N*Nseg;
+    noise_data->data->qpad = 0.0;
+
+    //TODO: Fix this hacky noise model frequency alignment
+    noise_data->data->fmin = gbmcmc_data->data->fmin + (double)qpad/T; //start noise model after padding
+    noise_data->data->fmin += (double)N/T; //shift start by one segment (root doesn't have gbmcmc)
+
+
+    noise_data->flags = malloc(sizeof(struct Flags));
+    memcpy(noise_data->flags, gbmcmc_data->flags, sizeof(struct Flags));
+
+    sprintf(noise_data->flags->runDir,"noise");
+    mkdir(noise_data->flags->runDir,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+    sprintf(dirname,"%s/chains",noise_data->flags->runDir);
+    mkdir(dirname,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+    sprintf(dirname,"%s/data",noise_data->flags->runDir);
+    mkdir(dirname,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+    noise_data->orbit = gbmcmc_data->orbit;
+    
+    alloc_data(noise_data->data, noise_data->flags);
+    
+    noise_data->model = malloc(sizeof(struct Model*)*gbmcmc_data->chain->NC);
+    noise_data->trial = malloc(sizeof(struct Model*)*gbmcmc_data->chain->NC);
+    
+    select_frequency_segment(noise_data->data, tdi_full, procID);
+}
+
+
+void initialize_noise_sampler(struct NoiseData *noise_data)
+{
+    /* Aliases to gbmcmc structures */
+    struct Flags *flags = noise_data->flags;
+    struct Orbit *orbit = noise_data->orbit;
+    struct Chain *chain = noise_data->chain;
+    struct Data *data   = noise_data->data;
+    struct Model **model = noise_data->model;
+    struct Model **trial = noise_data->trial;
+    
+    /* Get noise spectrum for data segment */
+    GalacticBinaryGetNoiseModel(data,orbit,flags);
+    
+    
+    /* Initialize parallel chain */
+    if(flags->resume)
+        initialize_chain(chain, flags, &data->cseed, "a");
+    else
+        initialize_chain(chain, flags, &data->cseed, "w");
+    
+    /* Initialize GBMCMC sampler state */
+    initialize_noise_state(data, orbit, flags, chain, model, trial);
+    
+    /* Set sampler counter */
+    noise_data->mcmc_step = -flags->NBURN;
+}
+
+void initialize_noise_state(struct Data *data, struct Orbit *orbit, struct Flags *flags, struct Chain *chain, struct Model **model, struct Model **trial)
+{
+    int NC = chain->NC;
+    int DMAX = flags->DMAX;
+    for(int ic=0; ic<NC; ic++)
+    {
+        
+        trial[ic] = malloc(sizeof(struct Model));
+        model[ic] = malloc(sizeof(struct Model));
+        
+        alloc_model(trial[ic],DMAX,data->N,data->Nchannel, data->NP, data->NT);
+        alloc_model(model[ic],DMAX,data->N,data->Nchannel, data->NP, flags->NT);
+                        
+        //set noise model
+        for(int j=0; j<flags->NT; j++) copy_noise(data->noise[j], model[ic]->noise[j]);
+        
+        // Form master model & compute likelihood of starting position
+        generate_noise_model(data, model[ic]);
+        generate_signal_model(orbit, data, model[ic], -1);
+        
+        if(!flags->prior)
+        {
+            model[ic]->logL     = gaussian_log_likelihood(orbit, data, model[ic]);
+            model[ic]->logLnorm = gaussian_log_likelihood_constant_norm(data, model[ic]);
+        }
+        else model[ic]->logL = model[ic]->logLnorm = 0.0;
+                
+    }//end loop over chains
+
+}
+
+int update_noise_sampler(struct NoiseData *noise_data)
+{
+    /* Aliases to gbmcmc structures */
+    struct Flags *flags = noise_data->flags;
+    struct Orbit *orbit = noise_data->orbit;
+    struct Chain *chain = noise_data->chain;
+    struct Data *data   = noise_data->data;
+    struct Model **model = noise_data->model;
+    struct Model **trial = noise_data->trial;
+
+    int NC = chain->NC;
+    
+    //For saving the number of threads actually given
+    int numThreads;
+    
+#pragma omp parallel num_threads(flags->threads)
+    {
+        int threadID;
+        //Save individual thread number
+        threadID = omp_get_thread_num();
+        
+        //Only one thread runs this section
+        if(threadID==0)  numThreads = omp_get_num_threads();
+        
+#pragma omp barrier
+        /* The MCMC loop */
+                
+#pragma omp barrier
+        // (parallel) loop over chains
+        for(int ic=threadID; ic<NC; ic+=numThreads)
+        {
+            
+            //loop over frequency segments
+            struct Model *model_ptr = model[chain->index[ic]];
+            struct Model *trial_ptr = trial[chain->index[ic]];
+            
+            //explicitly kill the signal model
+            //loop over time segments
+            for(int n=0; n<model_ptr->NT; n++)
+            {
+                for(int i=0; i<data->N*2; i++)
+                {
+                    model_ptr->tdi[n]->A[i] = 0.0;
+                    model_ptr->tdi[n]->E[i] = 0.0;
+                }
+            }
+            
+            //update likelihood
+            model_ptr->logL     = gaussian_log_likelihood(orbit, data, model_ptr);
+            model_ptr->logLnorm = gaussian_log_likelihood_constant_norm(data, model_ptr);
+
+            //evolve sampler
+            for(int steps=0; steps < 100; steps++)
+            {
+                noise_model_mcmc(orbit, data, model_ptr, trial_ptr, chain, flags, ic);
+            }
+        }// end (parallel) loop over chains
+         
+        
+        //Next section is single threaded. Every thread must get here before continuing
+#pragma omp barrier
+        if(threadID==0){
+            ptmcmc(model,chain,flags);
+            adapt_temperature_ladder(chain, noise_data->mcmc_step+flags->NBURN);
+            noise_data->mcmc_step++;
+        }
+        //Can't continue MCMC until single thread is finished
+#pragma omp barrier
+        
+    }// End of parallelization
+    
+    print_noise_state(data, model[chain->index[0]], chain->noiseFile[0], noise_data->mcmc_step);
+
+    if(noise_data->mcmc_step>=0 && noise_data->mcmc_step%data->downsample==0 && noise_data->mcmc_step/data->downsample < data->Nwave)
+    {
+        struct Model *model_ptr = model[chain->index[0]];
+
+        for(int n=0; n<data->N; n++)
+        {
+            data->S_pow[n][0][0][noise_data->mcmc_step/data->downsample] = model_ptr->noise[0]->SnA[n];
+            data->S_pow[n][1][0][noise_data->mcmc_step/data->downsample] = model_ptr->noise[0]->SnE[n];
+        }
+    }
+    return 1;
+}
