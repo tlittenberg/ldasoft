5a85c2e4a881f76fff08590a2853d273047800f4
Tyson Littenberg <tyson.littenberg@ligo.org>
2021-10-05 17:15:04 -0500

    trying again

---
+++ docs/html/structVBMCMCData.js
@@ -0,0 +1,15 @@
+var structVBMCMCData =
+[
+    [ "chain_vec", "structVBMCMCData.html#a1e6fb24d52c769d69e7f42903c18d6c2", null ],
+    [ "data_vec", "structVBMCMCData.html#ab1a4b2cbdfbb74ec25a67794f08ab3aa", null ],
+    [ "flags", "structVBMCMCData.html#a11c8d969f99cd564d23fabae5da0e42c", null ],
+    [ "mcmc_step", "structVBMCMCData.html#a53c60bbd3fb367e3ab604e644f42efdb", null ],
+    [ "model_vec", "structVBMCMCData.html#ad97d4f1d399c66411bac05a71a147cce", null ],
+    [ "nProc", "structVBMCMCData.html#a15edc608030d61101118e731d63d9512", null ],
+    [ "orbit", "structVBMCMCData.html#a58a6a3941b141d1871f516ec5fb77d6a", null ],
+    [ "prior_vec", "structVBMCMCData.html#a40c764a8cc4a9ebc4757f9b87144bd4f", null ],
+    [ "procID", "structVBMCMCData.html#a5a22dde0797a1a669076307810bcbcd0", null ],
+    [ "proposal_vec", "structVBMCMCData.html#a0f5e1cc8f5d26c01108b9d87b9f55b42", null ],
+    [ "status", "structVBMCMCData.html#abcd8d14ddd9020bfe4845bf155db5ad4", null ],
+    [ "trial_vec", "structVBMCMCData.html#ae9de47e1830d6a2f2c130c0ce399c146", null ]
+];
---
+++ docs/html/vb__mcmc_8c.js
@@ -0,0 +1,4 @@
+var vb__mcmc_8c =
+[
+    [ "main", "vb__mcmc_8c.html#a0ddf1224851353fc92bfbff6f499fa97", null ]
+];
