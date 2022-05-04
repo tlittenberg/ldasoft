26af0f1aaecff62bfa7e87868b312d0180f780e2
Tyson Littenberg <tyson.littenberg@ligo.org>
2022-05-04 11:04:14 -0500

    fewer fixed-d updates per gibbs step

--- globalfit/src/GalacticBinaryWrapper.c
+++ globalfit/src/GalacticBinaryWrapper.c
@@ -348,7 +348,7 @@ int update_gbmcmc_sampler(struct GBMCMCData *gbmcmc_data)
             
             for(int steps=0; steps<numSteps; steps++)
             {
-                for(int m=0; m<100; m++)
+                for(int m=0; m<10; m++)
                 {
                     galactic_binary_mcmc(orbit, data, model_ptr, trial_ptr, chain, flags, prior, proposal, ic);
                 }
--- globalfit/src/VerificationBinaryWrapper.c
+++ globalfit/src/VerificationBinaryWrapper.c
@@ -228,7 +228,7 @@ int update_vbmcmc_sampler(struct VBMCMCData *vbmcmc_data)
                 
                 for(int steps=0; steps<numSteps; steps++)
                 {
-                    for(int m=0; m<100; m++)
+                    for(int m=0; m<10; m++)
                     {
                         galactic_binary_mcmc(orbit, data_vec[n], model_ptr, trial_ptr, chain_vec[n], flags, prior_vec[n], proposal_vec[n], ic);
                     }//loop over MCMC steps
