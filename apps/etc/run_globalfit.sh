3abfab42146d80afb95684e5ba16116f81bac4e9
Tyson Littenberg <tyson.b.littenberg@gmail.com>
2023-12-11 14:24:32 -0600

    debugging

--- apps/etc/run_globalfit.sh
+++ apps/etc/run_globalfit.sh
@@ -1,19 +1,14 @@
-#!/bin/bash
-#SBATCH --job-name=LDC2a_Prod
-#SBATCH --output=slurm_12.out
-#SBATCH --error=slurm_12.err
-#SBATCH --partition=hpc-ondemand
-#SBATCH --ntasks=624
-#SBATCH --tasks-per-node=8
-#SBATCH --cpus-per-task=12
-
 globalfit="${GLASS_PREFIX}/global_fit"
-data="~/ldc/sangria/data/LDC2_sangria_training_v2.h5"
+data="${HOME}/ldc/sangria/data/LDC2_sangria_training_v2.h5"
 vgb="${GLASS_REPO}/apps/etc/hmcnc.dat"
 outdir="${GLASS_TEST}/globalfit"
 
 #path/to/search_sources.dat (contains starting point for MBH sampler)
-mbh="~/ldasoft/apps/etc/"
+mbh="${HOME}/ldasoft/apps/etc/"
+
+#Tobs=2621440
+#Tobs=7864320
+#padding=32
 
 fmin=0.0003
 samples=128
--- apps/etc/test_suite.sh
+++ apps/etc/test_suite.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export GLASS_PREFIX="/Users/tlittenb/ldasoft/reorg/bin"
-export GLASS_REPO="/Users/tlittenb/ldasoft"
-export GLASS_TEST="/Users/tlittenb/ldasoft/test"
+export GLASS_PREFIX="/Users/tyson/opt/bin"
+export GLASS_REPO="/Users/tyson/ldasoft"
+export GLASS_TEST="/Users/tyson/ldasoft/test"
 
+mkdir -p ${GLASS_TEST}
 
-#bash run_ucb.sh | tee ucb.out 
-#bash run_amcvn.sh | tee amcvn.out 
-#bash run_noise.sh | tee noise.out
-bash run_spline.sh | tee spline.out 
-#bash run_vgb.sh | tee vgb.out
-#bash run_globalfit.sh | tee globalfit.out 
+#bash run_ucb.sh | tee ${GLASS_TEST}/ucb.out 
+#bash run_amcvn.sh | tee ${GLASS_TEST}/amcvn.out 
+#bash run_noise.sh | tee ${GLASS_TEST}/noise.out
+#bash run_spline.sh | tee ${GLASS_TEST}/spline.out 
+#bash run_vgb.sh | tee ${GLASS_TEST}/vgb.out
+bash run_globalfit.sh | tee ${GLASS_TEST}/globalfit.out 
