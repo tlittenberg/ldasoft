a9fe08a8984bebc68460018796133667ce899ffc
Robert Rosati <robert.j.rosati@nasa.gov>
2025-05-22 11:27:52 -0500

    swapped in astrometry.net pix2ang

--- utils/src/glass_galaxy.c
+++ utils/src/glass_galaxy.c
@@ -353,7 +353,7 @@ void initialize_galaxy_modulation(struct GalaxyModulation *gm, struct Wavelets *
     for(i=0; i<gm->Npix; i++)
     {
         // get theta, phi coordinates of pixel (ecliptic)
-        pix2ang_ring(NSIDE,  i, &theta, &phi);
+        astropy_pix2ang_ring(NSIDE,  i, &theta, &phi);
         
         xe[0] = sin(theta)*cos(phi);
         xe[1] = sin(theta)*sin(phi);
@@ -485,12 +485,12 @@ static void alm2map(double *skyrecon, double **almR, double **almI, double ***Pl
     
     x = (double)(Npix)/(4.0*M_PI);
     
-    pix2ang_ring(NSIDE, 0, &thold, &phi);
+    astropy_pix2ang_ring(NSIDE, 0, &thold, &phi);
     i = 1;
     
     for(pix=0; pix < Npix; pix++)
     {
-        pix2ang_ring(NSIDE, pix, &theta, &phi);
+        astropy_pix2ang_ring(NSIDE, pix, &theta, &phi);
         
         if(fabs(theta-thold) > 1.0e-6)
         {
@@ -543,12 +543,12 @@ static void map2alm(double *sky, double **almR, double **almI, double ***Plm)
         Npix = 12*NSIDE*NSIDE;
         
         // we use this to keep track of which theta ring were are on (not very efficient)
-        pix2ang_ring(NSIDE, 0, &thold, &phi);
+        astropy_pix2ang_ring(NSIDE, 0, &thold, &phi);
         i = 1;
         
         for(pix=0; pix < Npix; pix++)
         {
-            pix2ang_ring(NSIDE, pix, &theta, &phi);
+            astropy_pix2ang_ring(NSIDE, pix, &theta, &phi);
             
             if(fabs(theta-thold) > 1.0e-6)
             {
